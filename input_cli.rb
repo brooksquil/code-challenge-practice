cedcfe66b18fdec55c4cea1dfded5a0e95d4a4ef
Brooke Wittenberg <bwittenberg.developer@gmail.com>
2021-10-11 11:08:41 -0500

    added

---
+++ input_cli.rb
@@ -0,0 +1,8 @@
+# puts "What is your name?" 
+# name = gets.chomp
+# puts "Hello, #{name}, nice to meet you!"
+
+puts "What is the input string?" 
+word = gets.chomp
+n = word.length
+puts "#{word} has #{n} characters."
